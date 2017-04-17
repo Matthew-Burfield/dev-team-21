a742a610c326de1237be66a6353bb9d2b004b03a
Matthew-Burfield <m1.burfield@gmail.com>
2017-04-17 14:05:26 +1000

    fixed some some code formatting

--- src/main.js
+++ src/main.js
@@ -7,96 +7,93 @@ import actionKeyPress from './keyHandler'
 var keys = [];
 var renderList = [mario];
 
-          function gameLoop() {
-            //handle key and check if mario is moving
-            actionKeyPress(keys,mario);
-            
-            // Clear the screen
-            constant.ctx.beginPath();
-            constant.ctx.rect(0, 0, constant.canvas.width, constant.canvas.height);
-            constant.ctx.fillStyle = "#2196F3";
-            constant.ctx.fill();
-            constant.ctx.closePath();
+function gameLoop() {
+  //handle key and check if mario is moving
+  actionKeyPress(keys,mario);
+  
+  // Clear the screen
+  constant.ctx.beginPath();
+  constant.ctx.rect(0, 0, constant.canvas.width, constant.canvas.height);
+  constant.ctx.fillStyle = "#2196F3";
+  constant.ctx.fill();
+  constant.ctx.closePath();
 
-            mario.velX *= constant.friction;
-            mario.velY += constant.gravity;
+  mario.velX *= constant.friction;
+  mario.velY += constant.gravity;
 
-            mario.grounded = false;
+  mario.grounded = false;
 
-            boxes.forEach(box => {
-              constant.ctx.drawImage(
-                box.image,
-                box.spriteX,
-                box.spriteY,
-                box.width,
-                box.height,
-                box.x,
-                box.y,
-                box.width,
-                box.height
-              );
-              var dir = collisionCheck(mario, box);
+  boxes.forEach(box => {
+    constant.ctx.drawImage(
+      box.image,
+      box.spriteX,
+      box.spriteY,
+      box.width,
+      box.height,
+      box.x,
+      box.y,
+      box.width,
+      box.height
+    );
+    var dir = collisionCheck(mario, box);
 
-              if (dir === "l" || dir === "r") {
-                mario.velX = 0;
-                mario.jumping = false; //TODO tweak it a bit, can jump from the wall.
-              } else if (dir === "b") {
-                mario.grounded = true;
-                mario.jumping = false;
-              } else if (dir === "t") {
-                mario.velY *= -1;
-              }
-            });
+    if (dir === "l" || dir === "r") {
+      mario.velX = 0;
+      mario.jumping = false; //TODO tweak it a bit, can jump from the wall.
+    } else if (dir === "b") {
+      mario.grounded = true;
+      mario.jumping = false;
+    } else if (dir === "t") {
+      mario.velY *= -1;
+    }
+  });
 
-            if(mario.grounded){
-                mario.velY = 0;
-            }
-            
-            mario.x += mario.velX;
-            mario.y += mario.velY;
+  if(mario.grounded){
+      mario.velY = 0;
+  }
+  
+  mario.x += mario.velX;
+  mario.y += mario.velY;
 
-            renderList.forEach(obj =>{
-                obj.render();
-            } );
-            requestAnimationFrame(gameLoop);
-          }
+  renderList.forEach(obj =>{
+      obj.render();
+  } );
+  requestAnimationFrame(gameLoop);
+}
 
-          const keyDownHandler = (e) => {
-            keys[e.keyCode] = true;
-          }
+const keyDownHandler = (e) => {
+  keys[e.keyCode] = true;
+}
 
-          const keyUpHandler = (e) => {
-            keys[e.keyCode] = false;
-          }
+const keyUpHandler = (e) => {
+  keys[e.keyCode] = false;
+}
 
-          
-          
+const boxes = [];
 
-          const boxes = [];
+for (let i = 0; i * blockSprite.width < constant.canvas.width; i += 1) {
+  const newBox = Object.create(blockSprite);
+  newBox.init({
+    x: i * blockSprite.width,
+    image: constant.blockSprites,
+  });
+  boxes.push(newBox);
+}
 
-          for (let i = 0; i * blockSprite.width < constant.canvas.width; i += 1) {
-            const newBox = Object.create(blockSprite);
-            newBox.init({
-              x: i * blockSprite.width,
-              image: constant.blockSprites,
-            });
-            boxes.push(newBox);
-          }
+for (let i = 0; i < 5; i += 1) {
+  const newBox = Object.create(blockSprite);
+  const startingX = 5 * 16;
+  const y = constant.canvas.height - (4 * 16);
+  newBox.init({
+    x: startingX + (i * 16),
+    y,
+    image: constant.blockSprites,
+  });
+  boxes.push(newBox);
+}
 
-          for (let i = 0; i < 5; i += 1) {
-            const newBox = Object.create(blockSprite);
-            const startingX = 5 * 16;
-            const y = constant.canvas.height - (4 * 16);
-            newBox.init({
-              x: startingX + (i * 16),
-              y,
-              image: constant.blockSprites,
-            });
-            boxes.push(newBox);
-          }
-
-          // Start the game loop as soon as the sprite sheet is loaded
-          window.addEventListener("load", gameLoop);
-          document.addEventListener('keydown', keyDownHandler);
-          document.addEventListener('keyup', keyUpHandler);
+// Start the game loop as soon as the sprite sheet is loaded
+window.addEventListener("load", gameLoop);
+document.addEventListener('keydown', keyDownHandler);
+document.addEventListener('keyup', keyUpHandler);
           
--- src/sprite.js
+++ src/sprite.js
@@ -1,120 +1,112 @@
-import {
-    canvas
-}
-from './constants';
+import { canvas } from './constants';
 
 export const movingSprite = {
-    init(options) {
-            this.context = options.context;
-            this.width = options.width;
-            this.height = options.height;
-            this.image = options.image;
-            this.jumping = false;
-            this.grounded = false;
-            this.moving = false;
-            this.speed = 3;
-            this.velX = 0;
-            this.velY = 0;
+  init(options) {
+    this.context = options.context;
+    this.width = options.width;
+    this.height = options.height;
+    this.image = options.image;
+    this.jumping = false;
+    this.grounded = false;
+    this.moving = false;
+    this.speed = 3;
+    this.velX = 0;
+    this.velY = 0;
 
-            // Sprite logic
-            this.frameIndex = 0;
-            this.tickCount = 0;
-            this.ticksPerFrame = options.ticksPerFrame || 0;
-            this.numberOfFrames = options.numberOfFrames || 1;
-            this.spriteStartX = options.spriteStartX;
-            this.spriteStartY = options.spriteStartY;
-            this.spriteSeparator = options.spriteSeparator;
-            this.spriteSeparator = options.spriteSeparator;
-            this.faceRight = true;
+    // Sprite logic
+    this.frameIndex = 0;
+    this.tickCount = 0;
+    this.ticksPerFrame = options.ticksPerFrame || 0;
+    this.numberOfFrames = options.numberOfFrames || 1;
+    this.spriteStartX = options.spriteStartX;
+    this.spriteStartY = options.spriteStartY;
+    this.spriteSeparator = options.spriteSeparator;
+    this.spriteSeparator = options.spriteSeparator;
+    this.faceRight = true;
 
-            // Canvas position
-            this.x = 0;
-            this.y = 0;
-        },
-        moveRight() {
-            if (this.velX < this.speed) {
-                this.moving = true;
-                this.velX++;
-                this.spriteStartY = 0;
-                this.update();
-            }
-        },
-        moveLeft() {
-            if (this.velX > -this.speed) {
-                this.moving = true;
-                this.velX--;
-                this.spriteStartY = 17;
-                this.update();
-            }
-        },
-        jump() {
-            if (!this.jumping && this.grounded) {
-                this.jumping = true;
-                this.grounded = false;
-                this.moving = true;
-                this.velY -= this.speed * 2;
-                this.update();
-            }
-        },
-        stop() {
-            this.moving = false;
-            this.update();
+    // Canvas position
+    this.x = 0;
+    this.y = 0;
+  },
+  moveRight() {
+    if (this.velX < this.speed) {
+      this.moving = true;
+      this.velX++;
+      this.spriteStartY = 0;
+      this.update();
+    }
+  },
+  moveLeft() {
+    if (this.velX > -this.speed) {
+      this.moving = true;
+      this.velX--;
+      this.spriteStartY = 17;
+      this.update();
+    }
+  },
+  jump() {
+    if (!this.jumping && this.grounded) {
+      this.jumping = true;
+      this.grounded = false;
+      this.moving = true;
+      this.velY -= this.speed * 2;
+      this.update();
+    }
+  },
+  stop() {
+    this.moving = false;
+    this.update();
+  },
+  render() {
 
-        },
-        render() {
+    // Draw the animation
+    this.context.drawImage(
+      this.image,
+      this.spriteStartX + (this.frameIndex * (this.width + this.spriteSeparator)),
+      this.spriteStartY,
+      this.width,
+      this.height,
+      this.x,
+      this.y,
+      this.width,
+      this.height
+    );
 
-            // Draw the animation
-            this.context.drawImage(
-                this.image,
-                this.spriteStartX + (this.frameIndex * (this.width + this.spriteSeparator)),
-                this.spriteStartY,
-                this.width,
-                this.height,
-                this.x,
-                this.y,
-                this.width,
-                this.height
-            );
+  },
+  update() {
+    this.tickCount += 1;
+    if (this.tickCount > this.ticksPerFrame) {
+      this.tickCount = 0;
 
-        },
-        update() {
-            this.tickCount += 1;
-            if (this.tickCount > this.ticksPerFrame) {
-                this.tickCount = 0;
+      // Check to see if the current frame index is in range
+      if (this.jumping) {
+        this.frameIndex = 5;
+      } else if(!this.moving) {
+        this.frameIndex = 0;
+      } else {
 
-                // Check to see if the current frame index is in range
-                 if (this.jumping) {
-                    this.frameIndex = 5;
-                     } 
-                     else if(!this.moving) {
-                    this.frameIndex = 0;
-                } else {
-                    if (this.frameIndex < this.numberOfFrames - 1) {
-                        // Go to the next frame
-                        this.frameIndex += 1;
-                    } else if (!this.singleAnimation){
-                        this.frameIndex = 0;
-                    }
-                }
+        if (this.frameIndex < this.numberOfFrames - 1) {
+          // Go to the next frame
+          this.frameIndex += 1;
+        } else if (!this.singleAnimation){
+          this.frameIndex = 0;
+        }
 
-
-            }
-        },
+      }
+    }
+  },
 };
 
 export const blockSprite = {
-   width: 16,
-   height: 16,
-   init(options) {
-   	this.x = options.x;
+  width: 16,
+  height: 16,
+  init(options) {
+    this.x = options.x;
     this.y = options.y || canvas.height - 16; //default to a floor block
-	this.spriteX = options.spriteX || 272; // default to brick
- 	this.spriteY = options.spriteY || 112, // default to brick
-       this.image = options.image;
-       this.breakable = options.breakable | false;
-       this.singleAnimation = options.singleAnimation | true;
-   },
-
-    
-
+    this.spriteX = options.spriteX || 272; // default to brick
+    this.spriteY = options.spriteY || 112, // default to brick
+    this.image = options.image;
+    this.breakable = options.breakable | false;
+    this.singleAnimation = options.singleAnimation | true;
+  },
 };
